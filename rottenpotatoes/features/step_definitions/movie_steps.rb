bb52b487e4a229784bdf29e30e08ec94eafdc1eb
ale7714 <aept7714@gmail.com>
2014-06-05 12:08:02 -0400

    hw3 part 3

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -11,9 +11,7 @@ end
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+   /#{e1}.*#{e2}/m =~ page.body
 end
 
 # Make it easier to express checking or unchecking several boxes at once
